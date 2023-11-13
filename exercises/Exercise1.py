c1722f367e2a163d66bebf0abd1e02a31bff287f
Hrushik Perumalla <59475008+hrushikp@users.noreply.github.com>
2023-11-13 18:57:57 +0100

    Update Exercise1.py

--- exercises/Exercise1.py
+++ exercises/Exercise1.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from sqlalchemy import String, TEXT, INTEGER, Float, DECIMAL
 
+#importing the data
 data = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit')
+
+#print the data to check
 print(df)
+
+#to chnage the datatypes
 columnTypes = {'column_1': INTEGER, 'column_2': String, 'column_3': String, 'column_4': String, 'column_5': String,
                'column_6': String, 'column_7': Float,'column_8': Float, 'column_9': INTEGER, 'column_10': Float,
                'column_11':TEXT,'column_12': String, 'geo_punkt': DECIMAL}
 
 data.to_sql('airports','sqlite:///airports.sqlite', if_exists='replace', index=False)
 
-#after running the oode, the output file will be generate with name "airports.sqlite".
+#after running the code, the output file will be generate with name "airports.sqlite".
